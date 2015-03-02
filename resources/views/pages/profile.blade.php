5dc71e63f53f0a72d9d669b3425509cda7f25e60
Rajat Hans <rajathans9@gmail.com>
2015-03-02 12:42:11 +0530

    profile changes

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 	<h5>Name 		: {{ $name }}</h5>
 	<h5>Roll no 	: {{ $enroll_no }}</h5>
+	<h5>Course  	: {{ $course }}</h5>
+	<h5>Majored in 	: {{ $majors }}</h5>
 	<h5>Email ID 	: {{ $email }}</h5>
 	<h5>Age			: {{ $age }}</h5>
 @stop
